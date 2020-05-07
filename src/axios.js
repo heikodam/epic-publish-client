5022e862afc7d60605032972a8e21339a491c59e
Heiko <heiko.damaske@gmail.com>
2020-05-07 16:17:27 +0200

    before clean up

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:4545',
     withCredentials: true
 });
 
 export default instance;
+
+// 
+// http://localhost:4545
